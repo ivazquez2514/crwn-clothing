3c717b2a13a754255d8093bb45bcf824448cc982
ivazquez2514 <isaacv2514@gmail.com>
2020-08-09 01:51:09 -0500

    rename cartItems in cart-dropdown component []

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -19,7 +19,7 @@ const CartDropdown = ({ cartItems }) => (
 );
 
 const mapStateToProps = ({ cart: { cartItems } }) => ({
-    selectCartItems
+    cartItems: selectCartItems
 });
 
 export default connect(mapStateToProps)(CartDropdown);
